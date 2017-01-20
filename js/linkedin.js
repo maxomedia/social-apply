a26eface3d4ff285b3b75ae5d0d3e5c9192244f5
Jessica Odermatt <jessica.odermatt@maxomedia.ch>
2017-01-20 10:30:51 +0100

    linkedin add docs

--- js/linkedin.js
+++ js/linkedin.js
@@ -2,6 +2,10 @@
 	Docs:
 	https://developer.linkedin.com/docs/getting-started-js-sdk
 	https://developer.linkedin.com/docs/signin-with-linkedin
+
+	Other:
+	Linkedin has limited the API Access r_fullprofile: http://stackoverflow.com/questions/29279057/accessing-r-fullprofile-after-new-linkedin-api-policy-changes
+	Apply for Linkedin Program: https://developer.linkedin.com/partner-programs/apply
 */
 
 window.Linkedin = {
